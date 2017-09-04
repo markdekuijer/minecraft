b974b412156d34dece34d3e60010969dce922e45
markdekuijer <31616400+markdekuijer@users.noreply.github.com>
2017-09-04 22:57:29 +0200

    Part1 Completed
    basic mesh render stuff

---
+++ minecraft remake/Assets/Scripts/Scripts/Blocks/BlockAir.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockAir : Block 
+{
+    public BlockAir() : base()
+    {
+
+    }
+
+    public override MeshData BlockData(Chunk chunk, int x, int y, int z, MeshData meshData)
+    {
+        return meshData;
+    }
+
+    public override bool IsSolid(Block.Direction direction)
+    {
+        return false;
+    }
+}
