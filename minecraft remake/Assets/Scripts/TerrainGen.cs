ee71b32f03d06963c2585a4ae1717b423e940c0d
markdekuijer <31616400+markdekuijer@users.noreply.github.com>
2017-09-08 15:13:35 +0200

    .
    .

---
+++ minecraft remake/Assets/Scripts/TerrainGen.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimplexNoise;
+
+public class TerrainGen
+{
+    public Chunk ChunkGen(Chunk chunk)
+    {
+    }
+
+    public static int GetNoise(int x, int y, int z, float scale, int max)
+    {
+        return Mathf.FloorToInt((Noise.Generate(x * scale, y * scale, z * scale) + 1f) * (max / 2f));
+    }
+}
